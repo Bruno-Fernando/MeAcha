68a7575bf4c3b242d76f129f53bfc016c1a90035
Bruno Fernando <bruno.silva@ccc.ufcg.edu.br>
2020-03-07 18:38:31 -0300

    Inicio das mudanças na definiçao do schema, anteriormente havia 2 schemas, sendo 1 deles para o post encontrado e o outro para post perdido. Essa forma introduzia redundancia alem de desperdiçar memoria. A partir de agora ha um post schema que define os atributos em comum e dois schemas para seus tipos especificos, perdido e encontrado.

---
+++ models/Post.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  authorName: {
+    type: String,
+    required: [true, "Add the author name"]
+  },
+  authorEmail: {
+    type: String,
+    required: [true, "Add the author email"]
+  },
+  title: {
+    type: String,
+    required: [true, "Add a title"]
+  },
+  description: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}, { discriminatorKey: 'posttype' });
+
+// TODO adicionar imagens ao post
+
+module.exports = mongoose.model("Post", PostSchema);
