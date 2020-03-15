07eec6121881358d8a06d085f0b0f075af19b2dd
Bruno Fernando <bruno.silva@ccc.ufcg.edu.br>
2020-03-15 15:59:39 -0300

    Adicionado verificacao regex nas entradas para diminuir a chance de um nosql inject

--- validationSchemas/posts.js
+++ validationSchemas/posts.js
@@ -1,26 +1,37 @@
 const Joi = require("@hapi/joi");
 
+const antiInject = new RegExp("^[^$&{|]*$");
+
 const basePostSchema = {
   authorName: Joi.string()
     .trim()
     .min(4)
     .max(15)
+    .pattern(antiInject)
     .required(),
   authorEmail: Joi.string()
     .trim()
     .min(6)
     .email()
+    .pattern(antiInject)
     .required(),
   title: Joi.string()
     .trim()
     .max(20)
+    .pattern(antiInject)
     .required(),
-  description: Joi.string().max(300)
+  description: Joi.string()
+    .max(300)
+    .pattern(antiInject)
 };
 
 const lostPostSchema = Joi.object({
   ...basePostSchema,
-  lostPlace: Joi.array().items(Joi.string().max(50)),
+  lostPlace: Joi.array().items(
+    Joi.string()
+      .max(50)
+      .pattern(antiInject)
+  ),
   bounty: Joi.number()
     .positive()
     .integer()
@@ -30,10 +41,12 @@ const foundPostSchema = Joi.object({
   ...basePostSchema,
   foundPlace: Joi.string()
     .trim()
+    .pattern(antiInject)
     .required()
     .max(280),
   meetingPlace: Joi.string()
     .trim()
+    .pattern(antiInject)
     .max(280)
 });
 
---
+++ validationSchemas/user.js
@@ -0,0 +1,25 @@
+const Joi = require("@hapi/joi");
+
+const antiInject = new RegExp("^[^$&{|]$");
+
+const userSchema = Joi.object({
+  name: Joi.string()
+    .trim()
+    .min(4)
+    .max(15)
+    .pattern(antiInject)
+    .required(),
+  email: Joi.string()
+    .trim()
+    .min(6)
+    .email()
+    .pattern(antiInject)
+    .required(),
+  password: Joi.string()
+    .min(8)
+    .max(20)
+    .pattern(antiInject)
+    .required()
+});
+
+module.exports = userSchema;
