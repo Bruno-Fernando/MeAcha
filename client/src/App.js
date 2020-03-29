f2d1811f0c38c927594d644ff8ffe5f8301a08b6
Bruno Fernando <bruno.silva@ccc.ufcg.edu.br>
2020-03-29 09:09:12 -0300

    Reiniciando projeto no react devido a problemas enfrendados durante o desenvolvimento, apos estudar e encontrar novas bibliotecas mais interessantes e novas formas de faze-lo, achei mais interessante começar novamente

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,7 @@
 import React from "react";
 
-import Nav from "./components/Nav";
-import Register from "./components/Register";
-
-import "./App.css";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-
 function App() {
-  return (
-  
-    <BrowserRouter>
-      <Nav />
-      <Switch>
-        <Route>
-          <Register path="/register" />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-    
-  );
+  return <div>hello</div>;
 }
 
 export default App;
