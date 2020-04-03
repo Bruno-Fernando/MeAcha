6b3c8ac29f58bb78d4a32bfcd927ea5ce8cc7f20
Bruno Fernando <bruno.silva@ccc.ufcg.edu.br>
2020-04-03 11:48:29 -0300

    Criado menu lateral com as mesmas funcionalidades que a barra de navegacao do desktop

---
+++ client/src/components/MobileSideMenu/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Divider from "@material-ui/core/Divider";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Avatar from "@material-ui/core/Avatar";
+import Grid from "@material-ui/core/Grid";
+
+import {
+  IoMdLogOut,
+  IoMdAddCircleOutline,
+  IoIosChatbubbles
+} from "react-icons/io";
+
+import useStyles from "./styles";
+
+export default function MobileSideMenu() {
+  const classes = useStyles();
+
+  return (
+    <Grid>
+      <Grid
+        container
+        justify="center"
+        alignItems="center"
+        className={classes.avatar}
+      >
+        <Link to="/user" className={classes.itemLink}>
+          <Avatar className={classes.perfilAvatar} />
+        </Link>
+      </Grid>
+
+      <Divider />
+      <List>
+        <Link to="new/post" className={classes.itemLink}>
+          <ListItem button>
+            <ListItemIcon>
+              <IoMdAddCircleOutline size="20px" />
+            </ListItemIcon>
+            <ListItemText primary="Criar Post" />
+          </ListItem>
+        </Link>
+
+        <Link to="/chat" className={classes.itemLink}>
+          <ListItem button>
+            <ListItemIcon>
+              <IoIosChatbubbles size="20px" />
+            </ListItemIcon>
+            <ListItemText primary="Chat" />
+          </ListItem>
+        </Link>
+
+        <ListItem button>
+          <ListItemIcon>
+            <IoMdLogOut size="20px" />
+          </ListItemIcon>
+          <ListItemText primary="Logout" />
+        </ListItem>
+      </List>
+      <Divider />
+    </Grid>
+  );
+}
